520aac1b073110a74ae2a86b82db0a0de0b0d15b
hemdee <sanusi5110@gmail.com>
2024-06-13 20:06:38 +0100

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,25 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/icons/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        Manrope: '"Manrope", sans-serif',
+      },
+      colors: {
+        primary: "#0257EF",
+        gray1: "#353B41",
+        gray2: "#42484F",
+        gray3: "#CCCCCC",
+        gray4: "#E9E7E7",
+        green: "#008000",
+        red: "#B60915",
+      },
+      width: {
+        fullscreen: "1440px",
       },
     },
   },
