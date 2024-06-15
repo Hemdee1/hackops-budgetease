247d9385ea31d01f798f96b75fe278b87935c530
hemdee <sanusi5110@gmail.com>
2024-06-15 11:41:10 +0100

    update password successful page

--- src/pages/create/password-successful.tsx
+++ src/pages/create/password-successful.tsx
@@ -1,9 +1,12 @@
+import AuthHeader from "@/components/authHeader";
 import MarkIcon from "@/icons/markIcon";
 import Link from "next/link";
 
 const Page = () => {
   return (
     <div className="py-24">
+      <AuthHeader />
+
       <div className="w-[600px] mt-20 max-w-full mx-auto border-gray4 border  rounded-3xl px-5 sm:px-10 py-20">
         <div className="flex flex-col gap-10 items-center">
           <MarkIcon />
