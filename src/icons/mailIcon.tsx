b3b5e056a2fb9be2949843f25e88ddb7ddab6695
hemdee <sanusi5110@gmail.com>
2024-06-15 11:36:07 +0100

    update icon

---
+++ src/icons/mailIcon.tsx
@@ -0,0 +1,17 @@
+const MailIcon = () => (
+  <svg
+    className="w-[70px] sm:w-[100px]"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M91.6664 16.6667H8.33301V83.3333H91.6664V16.6667ZM83.333 33.3333L49.9997 54.1667L16.6663 33.3333V25L49.9997 45.8333L83.333 25V33.3333Z"
+      fill="#0257EF"
+    />
+  </svg>
+);
+
+export default MailIcon;
