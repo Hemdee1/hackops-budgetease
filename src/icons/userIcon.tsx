bb61f615d31b7980dbf210f63e24c25fbfde59a0
hemdee <sanusi5110@gmail.com>
2024-06-17 10:32:15 +0100

    update user icon

---
+++ src/icons/userIcon.tsx
@@ -0,0 +1,22 @@
+const UserIcon = () => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="24" cy="24" r="24" fill="#EDF3FF" />
+    <path
+      d="M24 22C26.2091 22 28 20.2091 28 18C28 15.7909 26.2091 14 24 14C21.7909 14 20 15.7909 20 18C20 20.2091 21.7909 22 24 22Z"
+      fill="#0257EF"
+    />
+    <path
+      opacity="0.5"
+      d="M32 29.5C32 31.985 32 34 24 34C16 34 16 31.985 16 29.5C16 27.015 19.582 25 24 25C28.418 25 32 27.015 32 29.5Z"
+      fill="#0257EF"
+    />
+  </svg>
+);
+
+export default UserIcon;
